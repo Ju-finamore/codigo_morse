f6e40ab53cf18f78d459429811c76949fd188002
julia-finamore <juliamm_finamore@hotmail.com>
2023-01-31 22:45:02 -0300

    codei o metodo encode para palavras

--- encode.rb
+++ encode.rb
@@ -4,3 +4,7 @@ def encode(text)
   words = text.gsub(/[,.!?']/, '').split(' ')
   words.map { |word| encode_word(word) }.join('|')
 end
+
+def encode(word)
+  word.split('').map { |letter| MORSE[letter.upcase] }.join(' ')
+end
